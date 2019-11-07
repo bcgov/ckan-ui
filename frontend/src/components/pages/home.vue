8f320651ff51bd1b9ae8399290f6f3bd62ee3bd0
Brandon Sharratt <brandon@highwaythreesolutions.com>
2019-11-07 13:45:11 -0800

    added back carousel cycling

--- frontend/src/components/pages/home.vue
+++ frontend/src/components/pages/home.vue
@@ -10,7 +10,7 @@
             </v-col>
             <v-col cols=12>
                 <p>{{$tc('TrySearchingFor')}}</p>
-                <v-carousel interval="3000" xs6 :show-arrows="false" hide-delimiters height="40px">
+                <v-carousel interval="3000" cycle xs6 :show-arrows="false" hide-delimiters height="40px">
                     <v-carousel-item v-for="(item, i) in searchSuggestions" :key="i">
                         <p><a v-on:click="termClick">{{item}}</a></p>
                     </v-carousel-item>
