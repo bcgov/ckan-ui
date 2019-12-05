9c3de17510915bb30a651a74f3612aab756ded9f
Brandon Sharratt <brandon@highwaythreesolutions.com>
2019-12-05 08:38:05 -0800

    made user activity page make a little more gramatical sense

--- frontend/src/components/user/activityItem.vue
+++ frontend/src/components/user/activityItem.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container fluid>
-        <v-row wrap>
+        <v-row wrap class="pa-0 ma-0">
             <v-col cols=12>
-                You {{message}} - {{time}} ago
+                You {{message}} <a :href="link">{{linkName}}</a> - {{time}} ago
             </v-col>
         </v-row>
     </v-container>
@@ -17,7 +17,9 @@
         data() {
             return {
                 message: this.activity.activity_type,
-                time: this.timeSince(new Date(this.activity.timestamp))
+                time: this.timeSince(new Date(this.activity.timestamp)),
+                link: "",
+                linkName: "",
             }
         },
         methods: {
@@ -49,8 +51,30 @@
             }
         },
         mounted(){
-            if (this.message === "new user"){
-                this.message = "signed up"
+            switch(this.message){
+                case("new user"):
+                    this.message = "signed up";
+                    break;
+                case ("changed package"):
+                    this.message = "updated dataset";
+                    break;
+                case ("new package"):
+                    this.message = "created dataset";
+                    break;
+                case ("changed group"):
+                    this.message = "updated group";
+                    break;
+                case ("new group"):
+                    this.message = "created group";
+                    break;
+            }
+
+            if (this.message.indexOf('group') !== -1){
+                this.linkName = this.activity.data.group.title;
+                this.link = this.$router.matcher.match({name: 'group_view', params: { groupId: this.activity.data.group.name}}).path;
+            }else if (this.message.indexOf('dataset') !== -1){
+                this.link = this.$router.matcher.match({name: 'dataset_view', params: { datasetId: this.activity.data.package.name}}).path;
+                this.linkName = this.activity.data.package.title;
             }
         }
     }
