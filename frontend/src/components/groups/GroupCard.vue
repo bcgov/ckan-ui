c46c06f09e5efd27d9e1404ee8ca3ff976c97293
adodge-bcg <alan.dodge@gov.bc.ca>
2022-01-19 11:28:49 -0800

    Move api call for goup from group card to groups page, execute calls in sequence
    (cherry picked from commit ec177870b06fca0ef75ab5892ebe068f2a2e8974)

--- frontend/src/components/groups/GroupCard.vue
+++ frontend/src/components/groups/GroupCard.vue
@@ -32,8 +32,7 @@
 </template>
 
 <script>
-import {CkanApi} from '../../services/ckanApi'
-const ckanServ = new CkanApi()
+
 
 export default{
     name: 'GroupCard',
@@ -75,15 +74,6 @@ export default{
     updated() {
         window.snowplow('refreshLinkClickTracking');
     },
-
-    mounted() {
-      ckanServ.getGroup(this.id).then((data) => {
-
-        this.datasets = data.result.packages;
-
-        this.loading = false;
-      });
-    }
 
 }
 </script>
