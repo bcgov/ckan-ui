4d4479abf101049870574d0fc55921da7eea77c5
John Churchill <john@highwaythreesolutions.com>
2022-12-08 13:28:49 -0800

    New banner for planned outage

--- frontend/src/components/banner/Banner.vue
+++ frontend/src/components/banner/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-banner v-if="message" :color="colour" class="mx-md-n11 px-md-11 mt-n3" icon="mdi-information" elevation=0 tile single-line>
+  <v-banner v-if="message" :color="colour" class="mx-md-n11 px-md-11 mt-n3" icon="mdi-information" elevation=0 tile>
       {{message}}
       <template v-if="action" v-slot:actions>
           <v-btn :color="colour == 'banner_error' ? 'error' : 'secondary'" 
@@ -14,10 +14,10 @@
 export default {
     data() {
       return {
-        message: '',
-        action: '',
-        actionText: 'Give Feedback',
-        colour: 'banner_error' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
+        message: 'Tuesday, December 13, 2022, between 7:30 am and 1:00 pm, "BC Geographic Warehouse Custom Downloads" resources will be unavailable due to a routine upgrade.\n For any questions, please create a ticket with Data Systems and Services.',
+        action: 'https://dpdd.atlassian.net/servicedesk/customer/portal/1/group/1',
+        actionText: 'Create a Ticket',
+        colour: 'banner_warning' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
       }
     },
 }
