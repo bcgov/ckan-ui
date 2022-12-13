703910e093c38ffb7b2e94c6e5fa00a940eaf8c2
John Churchill <john@highwaythreesolutions.com>
2022-12-13 13:18:06 -0800

    Removing banner

--- frontend/src/components/banner/Banner.vue
+++ frontend/src/components/banner/Banner.vue
@@ -14,9 +14,9 @@
 export default {
     data() {
       return {
-        message: 'Tuesday, December 13, 2022, between 7:30 am and 1:00 pm, "BC Geographic Warehouse Custom Downloads" resources will be unavailable due to a routine upgrade.\n For any questions, please create a ticket with Data Systems and Services.',
-        action: 'https://dpdd.atlassian.net/servicedesk/customer/portal/1/group/1',
-        actionText: 'Create a Ticket',
+        message: '',
+        action: '',
+        actionText: '',
         colour: 'banner_warning' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
       }
     },
