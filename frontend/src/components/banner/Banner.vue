0c96d5a33dee3c2a8b0d256935017ed01a04410b
John Churchill <john.churchill@gov.bc.ca>
2023-05-04 13:56:56 -0700

    Revert "Added outage banner"
    This reverts commit 1c1a930c8f4aa8ebdfdce0d64c4703933a5b8c4c.

--- frontend/src/components/banner/Banner.vue
+++ frontend/src/components/banner/Banner.vue
@@ -1,13 +1,13 @@
 <template>
   <v-banner v-if="message" :color="colour" class="mx-md-n11 px-md-11 mt-n3" icon="mdi-information" elevation=0 tile>
       {{message}}
-      <template v-if="action" v-slot:actions>
+      <!-- <template v-if="action" v-slot:actions>
           <v-btn :color="colour == 'banner_error' ? 'error' : 'secondary'" 
               :href="action" tile class="white--text" target="_blank">
             {{actionText}}
           </v-btn>
-      </template>
-      <!-- <a :href="action" target="_blank">{{ actionText }}</a> -->
+      </template> -->
+      <a :href="action" target="_blank">{{ actionText }}</a>
   </v-banner>
 </template>
 
@@ -15,10 +15,10 @@
 export default {
     data() {
       return {
-        message: 'The BC Data Catalogue is currently experiencing a service outage. Our team is actively working to resolve the issue as quickly as possible. Thank you for your patience.',
-        action: '',
-        actionText: '',
-        colour: 'banner_error' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
+        message: 'Publishing your metadata in the BC Data Catalogue is a great way to ensure it complies with the new Metadata Standard and Guidelines. Find these on the ',
+        action: 'https://www2.gov.bc.ca/gov/content/data/about-the-metadata-standard',
+        actionText: 'Metadata Quick Facts page.',
+        colour: 'banner_info' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
       }
     },
 }
