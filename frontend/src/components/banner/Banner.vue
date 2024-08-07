5480d73c52ec3966baf2cf5f847dfa680c0b008f
John Churchill <john.churchill@gov.bc.ca>
2024-08-07 13:06:16 -0700

    Changed date in correct spot this time

--- frontend/src/components/banner/Banner.vue
+++ frontend/src/components/banner/Banner.vue
@@ -4,7 +4,7 @@
       Please note that there is a new version of BC Data Catalogue in our <a href="https://toyger.data.gov.bc.ca/">test environment</a>.
       The upgrade includes schema changes and if you or your clients use APIs to access, view or edit your datasets and resources, the schema changes will affect your current setup.
       Please contact us using our JSM channel <a href="https://dpdd.atlassian.net/servicedesk/customer/portal/1/group/1/create/18">Provide Feedback - Data Systems & Services - Jira Service Management (atlassian.net)</a>
-      for further guidance and test your applications using our <a href="http://toyger.data.gov.bc.ca/api">TEST API</a>. Testing window is open until August 06th.
+      for further guidance and test your applications using our <a href="http://toyger.data.gov.bc.ca/api">TEST API</a>. Testing window is open until August 15th.
     </span>
       <!-- <template v-if="action" v-slot:actions>
           <v-btn :color="colour == 'banner_error' ? 'error' : 'secondary'" 
