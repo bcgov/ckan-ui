53b29b4522e5921feb78aa8b3215d58d0881a9ce
John Churchill <john.churchill@gov.bc.ca>
2025-10-27 15:21:10 -0700

    Updated banner as requested

--- frontend/src/components/banner/Banner.vue
+++ frontend/src/components/banner/Banner.vue
@@ -1,9 +1,7 @@
 <template>
   <v-banner v-if="message" :color="colour" class="mx-md-n11 px-md-11 mt-n3" icon="mdi-information" elevation=0 tile>
       <span>
-        The BC Data Catalogue upgrade is successfully complete. Please find out more information at 
-        <a href="https://www2.gov.bc.ca/gov/content/data/finding-and-sharing/bc-data-catalogue/bc-catalogue-service-disruptions" target="_blank">BC Catalogue service disruptions</a>
-        page.
+        {{ message }}
       </span>
       <template v-if="action" v-slot:actions>
           <v-btn :color="colour == 'banner_error' ? 'error' : 'secondary'" 
@@ -19,7 +17,7 @@
 export default {
     data() {
       return {
-        message: 'Please note that there is a planned upgrade and a scheduled outage of the BC Data Catalogue from 12 pm on December 10th to 12 pm on December 11th, 2024. The catalogue pages, datasets and resources will be unavailable to users during the outage window. Please find out more information at BC Catalogue service disruptions page.',
+        message: 'Please note that the Distribution Service outage is resolved and service is active now. Users can access the service to download data through the catalogue.',
         action: '',
         actionText: '',
         colour: 'banner_info' // RED: banner_error, YELLOW: banner_warning, BLUE: banner_info
